e6c96f75b459f1d33f7f61cc565132d12d8b5858
prashantmagare <prashantmagare3010@gmail.com>
2022-06-01 23:29:48 +0530

    "Added - UC1_FirstName"

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Text.RegularExpressions;
 
 namespace UserRegistration
@@ -9,6 +8,20 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Regex User Registration Problem");
+            Console.WriteLine("Regex User Registration First Name");
+            Console.WriteLine("Enter first Name");
+
+            string text = Console.ReadLine();
+            //check the given input contain
+
+            if (Regex.Match(text, "^[A-Z][a-z]{3}$").Success)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
         }
     }
 }
