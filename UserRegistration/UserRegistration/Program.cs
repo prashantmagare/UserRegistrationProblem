fadacc38cf55620c24c76196f501189ae76deaae
prashantmagare <prashantmagare3010@gmail.com>
2022-06-01 23:38:36 +0530

    "Added - UC5_Password"

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -7,14 +7,14 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Regex UC 4 Mobile Number");
+            Console.WriteLine("Regex UC 5 Password");
 
-            Console.WriteLine("Enter Mobile Number");
+            Console.WriteLine("Enter Password");
 
-            string mobileNo = Console.ReadLine();
+            string password = Console.ReadLine();
 
 
-            if (Regex.Match(mobileNo, @"^([0-9]{2})\s([0-9]{10})$").Success)
+            if (Regex.Match(password, @"^([a-z]{5})([@]{1})([A-Z]{1})([0-9]{1})$").Success)
             {
                 Console.WriteLine("Success");
             }
